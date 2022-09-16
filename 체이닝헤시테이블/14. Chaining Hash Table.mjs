b57cb53f0940e77c11a2ffb9b7424c9c11e57348
Kyu Sung Sohn <rok.ksohn@gmail.com>
2022-09-16 20:33:27 +0900

    22.09.16 F

--- 체이닝헤시테이블/14. Chaining Hash Table.mjs
+++ 체이닝헤시테이블/14. Chaining Hash Table.mjs
@@ -19,7 +19,7 @@ ChainingHashTable.prototype.hashCode = function(key) {
   let hash = 0;
   for (let i = 0; i < key.length; i++) {
     hash += key.charCodeAt(i);
-  }
+  }  
   return hash % HASH_SIZE;
 }
 
