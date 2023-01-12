a3db6bf286a81b2b70c18013345a4ab32f12749f
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-01-12 21:16:29 +0900

    23.01.12 Th

--- TG.js
+++
@@ -1,9 +0,0 @@
-function getThisBinding() {
-    return this;
-}
-
-const thisArg = { a: 1 };
-console.log(getThisBinding()); // window 
-
-console.log(getThisBinding.apply(thisArg)); // { a: 1 }
-console.log(getThisBinding.call(thisArg)); // { a: 1 }
