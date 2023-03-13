0ad1158db88774cbe3a426d4ef69cf91995da77c
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-03-13 21:36:56 +0900

    Update React folder structure

---
+++ React/Practice/2. Clock (life cycle)/App.js
@@ -0,0 +1,13 @@
+import ClassClock from './components/ClassClock.js.js.js';
+import FunctionClock from './components/FunctionClock.js.js.js';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassClock /> */}
+      <FunctionClock />
+    </div>
+  );
+}
+
+export default App;
