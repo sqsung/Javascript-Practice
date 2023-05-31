7bbaaf361a5dbc9d232ac8579afaf2ee33cb9e3c
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-05-31 15:55:42 +0900

    Delete: 필요없는 파일 제거

--- TG.js
+++
@@ -1,4 +0,0 @@
-const examplePromise = new Promise((resolve) => resolve("fulfiled"))
-    .then((firstLineResult) => console.log(firstLineResult))
-    .catch((error) => console.log(error))
-    .finally(() => console.log("Bye!"));
